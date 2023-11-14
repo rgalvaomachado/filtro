e53b969c3af76d41517d182ae91874cd810f7d79
Rômulo Galvão Machado <52431713+rgalvaomachado@users.noreply.github.com>
2023-11-13 22:34:22 -0300

    fix icon

--- public/head.php
+++ public/head.php
@@ -2,7 +2,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>HFilter</title>
-    <link rel="icon" href="/public/img/logo-page.png">
+    <link rel="icon" href="/public/img/hubis-icon.png">
     <script src="/public/js/jquery-3.6.0.js"></script>
     <script src="/public/js/html2canvas.js"></script>
     <script src="https://kit.fontawesome.com/7ca1b2bc88.js" crossorigin="anonymous"></script>
