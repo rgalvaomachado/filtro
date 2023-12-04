f60719bde6d280841782988a22f2594012783f37
Rômulo Galvão Machado <52431713+rgalvaomachado@users.noreply.github.com>
2023-12-04 11:49:48 -0300

    mudanca nome

--- public/head.php
+++ public/head.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>HFilter</title>
+    <title>Filtro</title>
     <link rel="icon" href="/public/img/hubis-icon.png">
     <script src="/public/js/jquery-3.6.0.js"></script>
     <script src="/public/js/html2canvas.js"></script>
