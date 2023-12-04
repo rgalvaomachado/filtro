ede5f99681a68e05c69a73b185824ea34fc32004
Rômulo Galvão Machado <52431713+rgalvaomachado@users.noreply.github.com>
2023-12-04 12:10:36 -0300

    melhoria front

--- public/view/filtro/home.php
+++ public/view/filtro/home.php
@@ -14,63 +14,63 @@
 <div id="filtros">
     <?php
         $Filtro = new FiltroController();
-        $story = $Filtro->buscarTipoCliente(['tipo' => '1', 'cliente' => $_SESSION['cliente']]);
-        $q = json_decode($story);
-        if(count($q->filtros) > 0){ ?>
-            <div class="subtitulo">
-                <h2>Story</h2>
-            </div>
-            <div class="grid-container">
-                <?php foreach ($q->filtros as $filtro){ ?>
-                    <div class="grid-item">
-                        <div class="story">
-                            <a href="filtro?tipo=1&filtro=<?php echo $filtro->uniqid?>"> <img src="<?php echo $_ENV['DIRECTORY_FILTROS'].$filtro->uniqid.'.png'?>"></a>
-                        </div>
-                    </div>
-                <?php } ?> 
-            </div>
-        <?php } 
-    ?>
-
-    <?php
-        $Filtro = new FiltroController();
         $post_quadrado = $Filtro->buscarTipoCliente(['tipo' => '2', 'cliente' => $_SESSION['cliente']]);
-        $q = json_decode($post_quadrado);
-        if(count($q->filtros) > 0){ ?>
-            <div class="subtitulo">
-                <h2>Quadrado</h2>
-            </div>
-            <div class="grid-container">
-                <?php foreach ($q->filtros as $filtro){ ?>
-                    <div class="grid-item">
-                        <div class="post_quadrado">
-                            <a href="filtro?tipo=2&filtro=<?php echo $filtro->uniqid?>"> <img src="<?php echo $_ENV['DIRECTORY_FILTROS'].$filtro->uniqid.'.png'?>"></a>
-                        </div>
-                    </div>
-                <?php } ?> 
-            </div>
-        <?php } 
-    ?>
+        $quadrado = json_decode($post_quadrado);
 
-    <?php
         $Filtro = new FiltroController();
         $post_vertical = $Filtro->buscarTipoCliente(['tipo' => '3', 'cliente' => $_SESSION['cliente']]);
-        $q = json_decode($post_vertical);
-        if(count($q->filtros) > 0){ ?>
-            <div class="subtitulo">
-                <h2>Vertical</h2>
-            </div>
-            <div class="grid-container">
-                <?php foreach ($q->filtros as $filtro){ ?>
-                    <div class="grid-item">
-                        <div class="post_vertical">
-                            <a href="filtro?tipo=3&filtro=<?php echo $filtro->uniqid?>"> <img src="<?php echo $_ENV['DIRECTORY_FILTROS'].$filtro->uniqid.'.png'?>"></a>
-                        </div>
+        $vertical = json_decode($post_vertical);
+
+        if(count($quadrado->filtros) > 0 || count($vertical->filtros)){ ?>
+            <h2>Post</h2>
+            <div id="post">    
+                <?php if(count($quadrado->filtros) > 0){ ?>
+                    <h4>Quadrado</h4>
+                    <div class="grid-container">
+                        <?php foreach ($quadrado->filtros as $filtro){ ?>
+                            <div class="grid-item">
+                                <div class="post_quadrado">
+                                    <a href="filtro?tipo=2&filtro=<?php echo $filtro->uniqid?>"> <img src="<?php echo $_ENV['DIRECTORY_FILTROS'].$filtro->uniqid.'.png'?>"></a>
+                                </div>
+                            </div>
+                        <?php } ?> 
                     </div>
                 <?php } ?> 
-            </div>
+                <?php if(count($vertical->filtros) > 0){ ?>
+                    <h4>Vertical</h4>
+                    <div class="grid-container">
+                        <?php foreach ($vertical->filtros as $filtro){ ?>
+                            <div class="grid-item">
+                                <div class="post_vertical">
+                                    <a href="filtro?tipo=3&filtro=<?php echo $filtro->uniqid?>"> <img src="<?php echo $_ENV['DIRECTORY_FILTROS'].$filtro->uniqid.'.png'?>"></a>
+                                </div>
+                            </div>
+                        <?php } ?> 
+                    </div> 
+                <?php } ?> 
+            </div>       
         <?php } 
     ?>
+
+    <div id="story">
+        <?php
+            $Filtro = new FiltroController();
+            $story = $Filtro->buscarTipoCliente(['tipo' => '1', 'cliente' => $_SESSION['cliente']]);
+            $s = json_decode($story);
+            if(count($s->filtros) > 0){ ?>
+                <h2>Story</h2>
+                <div class="grid-container">
+                    <?php foreach ($s->filtros as $filtro){ ?>
+                        <div class="grid-item">
+                            <div class="story">
+                                <a href="filtro?tipo=1&filtro=<?php echo $filtro->uniqid?>"> <img src="<?php echo $_ENV['DIRECTORY_FILTROS'].$filtro->uniqid.'.png'?>"></a>
+                            </div>
+                        </div>
+                    <?php } ?> 
+                </div>
+            <?php } 
+        ?>
+    </div>
 </div>
 
 <?php //include_once('public/footer.php'); ?>
